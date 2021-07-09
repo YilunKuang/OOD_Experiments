535268b65a99bb02c333e1dc2acdc4c1fcc40fc9
YilunKuang <yk2516@nyu.edu>
2021-07-09 12:52:25 +0800

    Update perplexity_batched_gigaword_bart_cnn.py

--- BART_experiments/perplexity_batched_gigaword_bart_cnn.py
+++ BART_experiments/perplexity_batched_gigaword_bart_cnn.py
@@ -18,7 +18,7 @@ def main():
     # test = load_dataset("xsum",cache_dir='/scratch/yk2516/cache/', split='test[:]')
     # test = load_dataset("cnn_daily",cache_dir='/scratch/yk2516/cache/', split='test[:]')
     # test = load_dataset("cnn_dailymail", name='3.0.0',download_mode="force_redownload",split='test', cache_dir='/scratch/yk2516/cache/')
-    test = load_dataset(path="cnn_dailymail",name='3.0.0',split='test')
+    test = load_dataset(path="cnn_dailymail",name='3.0.0',split='test',cache_dir='/scratch/yk2516/cache/')
     
     print(test)
     print(model.config.max_length)
